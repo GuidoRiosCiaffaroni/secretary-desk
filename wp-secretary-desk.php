0dc6bf4921f3d053156cb403f865c098c7b808d7
wixancl <wixancl@gmail.com>
2021-11-26 11:03:01 -0300

    2021_11_26_11_03_01

--- wp-secretary-desk.php
+++ wp-secretary-desk.php
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) or die( '¡Sin trampas!' );
 
-require plugin_dir_path( __FILE__ ) . 'includes/metabox-p1.php';
+require plugin_dir_path( __FILE__ ) . 'includes/backend.php';
 
 /*Importa funciones de instalacion*/
 require_once plugin_dir_path( __FILE__ ) . 'includes/install.php';;
@@ -48,84 +48,9 @@ $wpbc_db_version = '1.1.0';
 global $sistname;
 $sistname = 'secretarydesk'; 
 
-/*funciones de instalacion require_once plugin_dir_path( __FILE__ ) . 'includes/install.php'; */
+/* inicio funciones de instalacion require_once plugin_dir_path( __FILE__ ) . 'includes/install.php'; */
 wpbc_install();
-
-
-/*
-function wpbc_install()
-{
-    global $wpdb;
-    global $wpbc_db_version;
-
-    $table_name = $wpdb->prefix . 'secretarydesk'; 
- 
-    $sql = "CREATE TABLE " . $table_name . " (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        nint VARCHAR (50) NOT NULL, 
-        date VARCHAR (100) NOT NULL,
-        depto_unid VARCHAR (100) NOT NULL,
-        nombres VARCHAR (100) NOT NULL,
-        apellido_paterno VARCHAR (100) NOT NULL,
-        apellido_materno VARCHAR (100) NOT NULL,
-        rut VARCHAR (100) NOT NULL,
-        email VARCHAR (100) NOT NULL,
-        perm_admin VARCHAR (100) NOT NULL,
-        fdo_legal VARCHAR (100) NOT NULL,
-        perm_parent VARCHAR (100) NOT NULL,
-        dias VARCHAR (100) NOT NULL,
-        desde VARCHAR (100) NOT NULL,
-        hasta VARCHAR (100) NOT NULL,
-        nombre_pdf VARCHAR (100) NOT NULL,
-        dir_archivo_externo VARCHAR (100) NOT NULL,
-        user_id int(11) NOT NULL,
-        user_name VARCHAR (100) NOT NULL,
-        status_id int(11) NOT NULL,
-        key_id VARCHAR (50) NOT NULL,
-        PRIMARY KEY  (id)
-    );";
-
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-
-    add_option('wpbc_db_version', $wpbc_db_version);
-
-    $installed_ver = get_option('wpbc_db_version');
-    if ($installed_ver != $wpbc_db_version) {
-        $sql = "CREATE TABLE " . $table_name . " (
-            id int(11) NOT NULL AUTO_INCREMENT,
-            nint VARCHAR (50) NOT NULL, 
-            date VARCHAR (100) NOT NULL,
-            depto_unid VARCHAR (100) NOT NULL,
-            nombres VARCHAR (100) NOT NULL,
-            apellido_paterno VARCHAR (100) NOT NULL,
-            apellido_materno VARCHAR (100) NOT NULL,
-            rut VARCHAR (100) NOT NULL,
-            email VARCHAR (100) NOT NULL,
-            perm_admin VARCHAR (100) NOT NULL,
-            fdo_legal VARCHAR (100) NOT NULL,
-            perm_parent VARCHAR (100) NOT NULL,
-            dias VARCHAR (100) NOT NULL,
-            desde VARCHAR (100) NOT NULL,
-            hasta VARCHAR (100) NOT NULL,
-            nombre_pdf VARCHAR (100) NOT NULL,
-            dir_archivo_externo VARCHAR (100) NOT NULL,
-            user_id int(11) NOT NULL,
-            user_name VARCHAR (100) NOT NULL,
-            status_id int(11) NOT NULL,
-            key_id VARCHAR (50) NOT NULL,
-            PRIMARY KEY  (id)
-        );";        
-
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
-
-        update_option('wpbc_db_version', $wpbc_db_version);
-    }
-}
-*/
+/* inicio funciones de instalacion require_once plugin_dir_path( __FILE__ ) . 'includes/install.php'; */
 
 register_activation_hook(__FILE__, 'wpbc_install');
 
