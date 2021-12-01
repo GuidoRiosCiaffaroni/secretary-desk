4e3d13a72363afd0be46961c6c6e0f6ca7bf65af
wixancl <wixancl@gmail.com>
2021-12-01 14:50:07 -0300

    2021_12_01_14_50_07

--- includes/frontend_buscar.php
+++ includes/frontend_buscar.php
@@ -9,8 +9,34 @@ add_shortcode('kfp_ShotCondeBuscar_form', 'Kfp_Buscar_form');
 function Kfp_Buscar_form() 
 {
   global $wpdb;
-  $registros = $wpdb->get_results( "SELECT nombre, email FROM wp_secretarydesk" );
-  echo "id: " . $registros[0]->id . ", nint: " . $registros[0]->nint . "<br/>";
+  $registros = $wpdb->get_results( "SELECT * FROM wp_secretarydesk" );
+
+
+
+        // nombre de los campos de la tabla
+        foreach ($registros as $registros) {
+            $result .= 
+            '
+            <tr>
+                <td>'.$registros->id.'</td>
+                <td>'.$registros->nint.'</td>
+            </tr>
+            ';
+        }
+
+
+
+        $template = '<table class="table-data">
+                      <tr>
+                        <th>ID</th>
+                        <th>nint</th>
+                      </tr>
+                      {data}
+                    </table>';
+
+        return $content.str_replace('{data}', $result, $template);
+
+
   }
 // Ejecutamos nuestro funcion en WordPress
 add_action('wp', 'leer_wpdb');
